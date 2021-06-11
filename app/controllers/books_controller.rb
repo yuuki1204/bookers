4cd61c33934d9bd5ec08645a54a3080bc9d47ba0
yuuki1204 <vocalistyuuki@gmail.com>
2021-06-11 09:03:31 +0000

    3rd commit

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,13 +1,30 @@
 class BooksController < ApplicationController
   def index
+    @book = Book.new
+
   end
 
   def show
+    @book = Book.find(params[:id])
   end
 
-  def new
+  def create
+   book = Book.new(book_params)
+   book.save
+   redirect_to book_path(book.id)
   end
 
   def edit
   end
+
+  def top
+  end
+
+  private
+  # ストロングパラメータ
+  def book_params
+    params.require(:book).permit(:title, :body)
+  end
+
+
 end
