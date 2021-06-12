f2767565e606b899a00ce6f711cbe7ded76675ce
yuuki1204 <vocalistyuuki@gmail.com>
2021-06-12 10:58:57 +0000

    commit

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,7 +1,8 @@
 class BooksController < ApplicationController
-  def index
-    @book = Book.new
 
+  def index
+  @book = Book.new
+  @books = Book.all
   end
 
   def show
@@ -15,10 +16,23 @@ class BooksController < ApplicationController
   end
 
   def edit
+    @book = Book.find(params[:id])
   end
 
   def top
   end
+
+   def update
+   book = Book.find(params[:id])
+   book.update(book_params)
+   redirect_to book_path(book.id)
+   end
+
+   def destroy
+     book = Book.find(params[:id])
+     book.destroy
+     redirect_to books_path
+   end
 
   private
   # ストロングパラメータ
