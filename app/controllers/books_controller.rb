ac99a8b1a7f8a8e2f618b31e7a670ea708ae12e6
yuuki1204 <vocalistyuuki@gmail.com>
2021-06-13 08:49:57 +0000

    5th commit

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -9,11 +9,18 @@ class BooksController < ApplicationController
     @book = Book.find(params[:id])
   end
 
+
   def create
-   book = Book.new(book_params)
-   book.save
-   redirect_to book_path(book.id)
+    @book = Book.new(book_params)
+   if @book.save
+     flash[:notice] = "Book was successfully created."
+    redirect_to book_path(@book.id)
+   else
+     @books = Book.all
+    render :index
+   end
   end
+
 
   def edit
     @book = Book.find(params[:id])
@@ -23,9 +30,14 @@ class BooksController < ApplicationController
   end
 
    def update
-   book = Book.find(params[:id])
-   book.update(book_params)
-   redirect_to book_path(book.id)
+  @book = Book.find(params[:id])
+   if @book.update(book_params)
+     flash[:notice] = "Book was successfully update."
+    redirect_to book_path(@book.id)
+   else
+
+    render :edit
+   end
    end
 
    def destroy
@@ -37,7 +49,7 @@ class BooksController < ApplicationController
   private
   # ストロングパラメータ
   def book_params
-    params.require(:book).permit(:title, :body)
+    params.require(:book).permit(:title, :body,)
   end
 
 
